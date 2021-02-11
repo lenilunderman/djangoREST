d179aa883ee483829ebb2ece53ec2a8047f31e9e
Leni Lunderman <lenilunderman@gmail.com>
2021-02-11 09:50:57 -0500

    Creating Post API and Upvoting Posts

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Vote
 
 class PostSerializer(serializers.ModelSerializer):
+    poster = serializers.ReadOnlyField(source='poster.username')
+    poster_id = serializers.ReadOnlyField(source='poster.id')
     class Meta:
         # tell what model to use
         model = Post
         # define the fields that you would like to serialize.
-        fields = ['id','title','url','poster','created']
+        fields = ['id','title','url','poster','poster_id','created']
+
+
+class VoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        # tell what model to use
+        model = Vote
+        # define the fields that you would like to serialize.
+        fields = ['id']
--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,44 @@
 from django.shortcuts import render
 # imports related to rest_framework
-from rest_framework import generics
+from rest_framework import generics, permissions
+# import the error validation from rest
+from rest_framework.exceptions import ValidationError
 # import of the .models, table Post
-from .models import Post
+from .models import Post, Vote
 # import the serializer class to be used
-from .serializers import PostSerializer
+from .serializers import PostSerializer, VoteSerializer
+
 # Create your views here.
-class PostList(generics.ListAPIView):
+class PostList(generics.ListCreateAPIView):
     # display all the posts using query set
     queryset = Post.objects.all()
     # tell what serializer to use for this class
     serializer_class = PostSerializer
+    # import permissions from the rest framework.
+    # that allow users to see the info if they are not loged in or write if they are loged in
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    # Create the function and use the special name 'perform_create
+    def perform_create(self, serializer):
+        serializer.save(poster=self.request.user)
+
+class VoteCreate(generics.CreateAPIView):
+    # display all the posts using query set
+    queryset = Post.objects.all()
+    # tell what serializer to use for this class
+    serializer_class = VoteSerializer
+    # import permissions from the rest framework.
+    # someone has to be authenticated to read this
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        return Vote.objects.filter(voter=user, post=post)
+
+    # Create the function and use the special name 'perform_create
+    def perform_create(self, serializer):
+        # check if something came back from the queryset
+        if self.get_queryset().exists():
+            raise ValidationError('You have already voted for this post.')
+        serializer.save(voter=self.request.user, post=Post.objects.get(pk=self.kwargs['pk']))
